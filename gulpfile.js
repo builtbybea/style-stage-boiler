1aa687e7d004886f33a7b0bd7a33af3d01456935
harriet-mcmahon <harrietsotheremail@gmail.com>
2020-10-06 12:46:27 +0100

    init(gulp) Gulp Stuff
    Adding gulp + index.html

---
+++ gulpfile.js
@@ -0,0 +1,32 @@
+const gulp = require('gulp'),
+    sass = require('gulp-sass'),
+    browserSync = require('browser-sync');
+
+
+gulp.task('scss', function(){
+  return gulp.src('app/scss/**/*.scss')
+    .pipe(sass())
+    .pipe(gulp.dest('app/css'))
+    .pipe(browserSync.reload({stream: true}))
+});
+
+gulp.task('html', function(){
+  return gulp.src('app/*.html')
+  .pipe(browserSync.reload({stream: true}))
+});
+
+gulp.task('browser-sync', function() {
+    browserSync.init({
+      server: {
+        baseDir: "app/"
+      },
+      notify: false
+  });
+});
+
+gulp.task('watch', function(){
+  gulp.watch('app/scss/**/*.scss', gulp.parallel('scss'));
+  gulp.watch('app/*.html', gulp.parallel('html'))
+});
+
+gulp.task('default', gulp.parallel('scss', 'browser-sync', 'watch'));
